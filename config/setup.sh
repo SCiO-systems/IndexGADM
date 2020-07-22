9eb0554f9f5386b9ec9650be3d568765771a113d
KyriakosZorbas <15860064+KyriakosZorbas@users.noreply.github.com>
2020-07-22 11:46:15 +0300

    Update setup.sh

--- config/setup.sh
+++ config/setup.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 
 # for some reason even when port 9200 is open Elasticsearch is unable to be accessed as authentication fails
 # a few seconds later it works
-until $(curl -sSf -XGET --insecure --user xxxxx:xxxxx 'http://localhost:9200/_cluster/health?wait_for_status=yellow' > /dev/null); do
+until $(curl -sSf -XGET --insecure --user elastic:changeme 'http://localhost:9200/_cluster/health?wait_for_status=yellow' > /dev/null); do
     printf 'AUTHENTICATION ERROR DUE TO X-PACK, trying again in 5 seconds \n'
     sleep 5
 done
@@ -14,7 +14,7 @@ done
 # create a new index with the settings in es_index_config.json
 
 
-curl -v --insecure --user xxxx:xxxx -XPUT "localhost:9200/_snapshot/my_backup" -H 'Content-Type: application/json' -d'
+curl -v --insecure --user elastic:changeme -XPUT "localhost:9200/_snapshot/my_backup" -H 'Content-Type: application/json' -d'
 {
   "type": "fs",
   "settings": {
